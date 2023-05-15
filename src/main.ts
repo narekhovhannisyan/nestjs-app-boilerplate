e4be771ceeee36705919f88ff4955fe3de785d50
Narek Hovhannisyan <narekgilmour@gmail.com>
2023-05-15 22:08:00 +0400

    root: basic setup.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+import { CONFIG } from './config';
+import { OpenAPIBuilder } from './config/swagger';
+
+const { API_VERSION, PORT } = CONFIG;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  /**
+   * Set API version.
+   */
+  app.setGlobalPrefix(API_VERSION);
+
+  /**
+   * Setup Swagger module.
+   */
+  SwaggerModule.setup('docs', app, OpenAPIBuilder(app), {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  await app.listen(PORT);
 }
 bootstrap();
